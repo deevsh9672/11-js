7399b9102220ffbda929776290813f03b294af1d
devesh kumar meena <129925418+deevsh9672@users.noreply.github.com>
2023-11-21 14:51:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function convert() {
+    // alert("")
+    const tempinc = document.querySelector("#tempinc").value;
+    const tempinf = tempinc * (9 / 5) + 32;
+  
+    document.querySelector("span").innerText = tempinf;
+  }
+  
