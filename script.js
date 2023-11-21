38ba840e92bff5b80a20852acfd610fb54073fdb
devesh kumar meena <129925418+deevsh9672@users.noreply.github.com>
2023-11-21 16:21:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function convert() {
-    // alert("")
-    const tempinc = document.querySelector("#tempinc").value;
-    const tempinf = tempinc * (9 / 5) + 32;
-  
-    document.querySelector("span").innerText = tempinf;
-  }
+functiom calculate(){
+const cartype= document.querySelector('Input[name="cartype"]:clicked').value;
+
+    const days= document.querySelector("#days").value;
+
+    let rent =0;
+    switch(cartype){
+        case "ecomnory":
+            rent= 400*days;
+            break;
+
+        case "midsize":
+            rent = 10000*days;
+            break;
+        case "luxury":
+            rent= 20000*days;
+            break;
+
+            defualt:
+            alert("please selector all rent")
+             break;
+    }
+     document.querySelector("span").innerText=rent;
+}
+            
   
